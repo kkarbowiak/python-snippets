c03a48c64091249010945911b38d73e7e8ddf5a1
kkarbowiak <kkarbowiak@localhost>
2017-09-19 11:53:16 +0200

    Change format of logged messages to align messages vertically regardless of level name

--- logging_usage.py
+++ logging_usage.py
@@ -2,7 +2,7 @@ import logging
 
 
 def main():
-    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.NOTSET, datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s: %(message)s', level=logging.NOTSET, datefmt='%Y-%m-%d %H:%M:%S')
     logging.debug('this is a debug message')
     logging.info('this is an info message')
     logging.warning('this is a warning message')
